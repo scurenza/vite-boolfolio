28f76ec475a4e724e6a8fcceb55f739892635739
scurenza <fedechiar@hotmail.it>
2023-01-23 17:25:31 +0100

    Add: pagination

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,17 +11,31 @@ export default {
         return {
             store,
             projects: [],
+            currentPage: 1,
+            lastPage: null,
+            totalProjects: null,
             loading: false
         };
     },
     created() {
-        this.getProjects();
+        this.getProjects(1);
     },
     methods: {
-        getProjects() {
+        getProjects(page) {
+            const options = {
+                params: {
+                    page
+                }
+            }
+
             this.loading = true;
-            axios.get(`${this.store.apiBaseUrl}/api/projects`).then(resp => {
-                this.projects = resp.data.results;
+
+            axios.get(`${this.store.apiBaseUrl}/api/projects`, options).then(resp => {
+                // console.log(resp.data.results);
+                this.projects = resp.data.results.data;
+                this.currentPage = resp.data.results.current_page;
+                this.lastPage = resp.data.results.last_page;
+                this.totalProjects = resp.data.results.total;
                 this.loading = false;
             });
         }
@@ -37,8 +51,27 @@ export default {
         <AppLoader v-if="loading" />
         <div v-else class="row justify-content-center">
             <div class="col-10-col-md-9 col-lg-8">
+                <p class="text-end">Totale {{ totalProjects }} progetti trovati</p>
                 <ProjectCard :project="project" v-for="project in projects" :key="project.id" />
+
+                
+                <!-- pages navigation -->
+                <nav class="navigation d-flex justify-content-between">
+                    <div>
+                        page {{ currentPage }} in {{ lastPage }}
+                    </div>
+                    <div>
+                        <a class="btn btn-success me-2" :class="currentPage === 1 ? 'disabled' : ''" href=""
+                            @click.prevent="getProjects(currentPage - 1)">Back</a>
+                        <a class="btn btn-success" :class="{ 'disabled': currentPage === lastPage }" href=""
+                            @click.prevent="getProjects(currentPage + 1)">Next</a>
+                    </div>
+
+                </nav>
             </div>
         </div>
     </div>
 </template>
+
+<style lang="scss">
+</style>
